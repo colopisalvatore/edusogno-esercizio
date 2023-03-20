81c4f334299ebc0da16781170731ae776426c718
Salvatore Colopi <salvatorecolopi1989@gmail.com>
2023-03-20 17:24:48 +0100

    fix header

--- assets/partials/header.php
+++ assets/partials/header.php
@@ -1,5 +1,5 @@
 <header>
     <div class="logo">
-        <img id="img-logo" src="assets/img/logo.svg" alt="Immagine che rappresenta il logo di Edusogno">
+        <img id="logo" src="./assets/img/logo.svg" alt="Immagine che rappresenta il logo di Edusogno">
     </div>
 </header>
