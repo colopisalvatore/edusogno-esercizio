efec9f49fe79f251572ef8a2889840e8c4685c78
Salvatore Colopi <salvatorecolopi1989@gmail.com>
2023-03-21 18:20:47 +0100

    add head.php

---
+++ assets/partials/head.php
@@ -0,0 +1,24 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">  
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <!--  Main CSS File -->
+    <link href="./assets/styles/style.css" rel="stylesheet">
+
+    <!-- Main JS File -->
+    <script src="./assets/js/script.js" defer></script>
+
+
+    <title>Edusogno</title>
+</head>
