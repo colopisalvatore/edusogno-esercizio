3019dfd756c44fc58a8058c0b46cad4132daee01
Salvatore Colopi <salvatorecolopi1989@gmail.com>
2023-03-21 17:20:30 +0100

    add var and logic for logout

--- assets/partials/header.php
+++ assets/partials/header.php
@@ -1,5 +1,11 @@
+<?php session_start(); ?>
 <header>
     <div class="logo">
         <img id="logo" src="./assets/img/logo.svg" alt="Immagine che rappresenta il logo di Edusogno">
     </div>
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <span id="logoutButtonContainer">
+            <a href="logout.php" class="logout-button">Logout</a>
+        </span>
+    <?php endif; ?>
 </header>
---
+++ assets/partials/logout.php
@@ -0,0 +1,14 @@
+<?php
+// Avvia la sessione
+session_start();
+
+// Rimuovi tutte le variabili di sessione
+$_SESSION = array();
+
+// Distruggi la sessione
+session_destroy();
+
+// Reindirizza l'utente alla pagina di login
+header("location: login.php");
+exit;
+?>
