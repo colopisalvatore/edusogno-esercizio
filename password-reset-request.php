6cf1678e4a3c401c8d37ac39222b9785794b8cba
Salvatore Colopi <salvatorecolopi1989@gmail.com>
2023-03-21 20:11:22 +0100

    fix

--- password-reset-request.php
+++ password-reset-request.php
@@ -7,14 +7,6 @@
     <h1>Richiedi una nuova password temporanea</h1>
     <!-- Mostra il messaggio di errore/successo se presente -->
     <?php include './assets/partials/message.php' ?>
-    <?php if (isset($_SESSION['message'])) : ?>
-        <div class="error-message">
-            <?php
-            echo $_SESSION['message'];
-            unset($_SESSION['message']);
-            ?>
-        </div>
-    <?php endif; ?>
     <section class="form">
         <div class="login-form">
             <form action="./assets/partials/send-temporary-password.php" method="POST">
